3a9958b2ef2d56bf46893b33a2ec7b56d79623aa
atwanzireT <tatwanzire@gmail.com>
2022-05-23 15:28:27 +0300

    Going through Get method

--- api.js
+++ api.js
@@ -7,9 +7,20 @@ app.get('/',(req, res) => {
     res.send("<div><h1>Go to APIs </h1><a href='/api/products/'>Products</a></div>")
 })
 
+// getting all products
 app.get('/api/products/', (req, res) =>{
-    res.send(products);
-    console.log(products);
+    const newProduct = products.map((product) => {
+        const {id, name, price, image} = product;
+        return {id, name, price, image}
+    })
+    res.json(newProduct);
+    console.log(newProduct);
+})
+
+// getting single product
+app.get('/api/products/1', (req,res)=>{
+    const singleProduct = products.find((product) => product.id === 1)
+    res.json(singleProduct);
 })
 
 app.listen(5000, () => {
--- data.js
+++ data.js
@@ -2,23 +2,27 @@
 const products = [
     {
         "id" : 1,
-        "name" : "watch",
+        "name" : "fedora",
         "price" : 10000,
+        "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/A_fedora_hat%2C_made_by_Borsalino.jpg/1200px-A_fedora_hat%2C_made_by_Borsalino.jpg",
     },
     {
         "id" : 2,
-        "name" : "TV",
+        "name" : "fedora XL",
         "price" : 17000,
+        "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/A_fedora_hat%2C_made_by_Borsalino.jpg/1200px-A_fedora_hat%2C_made_by_Borsalino.jpg",
     },
     {
         "id" : 3,
-        "name" : "watch",
+        "name" : "fedora SM",
         "price" : 10000,
+        "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/A_fedora_hat%2C_made_by_Borsalino.jpg/1200px-A_fedora_hat%2C_made_by_Borsalino.jpg",
     },    
     {
         "id" : 4,
-        "name" : "phone",
+        "name" : "fedora L",
         "price" : 14000,
+        "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/A_fedora_hat%2C_made_by_Borsalino.jpg/1200px-A_fedora_hat%2C_made_by_Borsalino.jpg",
     }
 ]
 
