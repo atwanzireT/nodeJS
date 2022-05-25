391037f93a8ec8a716f1f51656fb6ca27bd31f2c
atwanzireT <tatwanzire@gmail.com>
2022-05-25 11:48:11 +0300

    worked on express third party middlewares

--- express/middleware.js
+++ express/middleware.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const logger = require('./logger');
 const authorize = require('./authorize');
+const morgan = require("morgan");
 
 // req  => middleware => res
 
 //  1: use vs route
 //  2: Options -Our own / express / third Party
-app.use([logger, authorize]);
+// app.use([logger, authorize]);
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
     res.send("<div><h1>Home Page ...</h1><a href = '/about/'>About</a></div>");
