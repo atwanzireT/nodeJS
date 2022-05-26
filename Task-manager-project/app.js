9248df8926f33f370b671626bc1b139adc259da1
atwanzireT <tatwanzire@gmail.com>
2022-05-26 15:38:44 +0300

    worked on thr task manager routes

---
+++ Task-manager-project/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const tasks = require("./Routes/tasks");
+
+// middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
+
+app.use('/api/v1/tasks', tasks);
+
+
+// const port = 5000;
+app.listen(5000, () => {
+    console.log("Connected ...")
+});
