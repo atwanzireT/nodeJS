431616b1ca171e6ba6a3f528e9bbf668379113fe
atwanzireT <tatwanzire@gmail.com>
2022-05-28 21:28:24 +0300

    configured models

--- Task-manager-project/modals/tasks.js
+++ Task-manager-project/modals/tasks.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-    name:String,
-    completed: Boolean,
+    name:{
+        type : String,
+        required: [true, "Must Provide ..."],
+        trim:true,
+        maxlength: [45, "Name can not be more than 20 characters ..."]
+    },
+    completed: {
+        type:Boolean,
+        default: false,
+    },
 })
 
 module.exports = mongoose.model('Task', TaskSchema); 
