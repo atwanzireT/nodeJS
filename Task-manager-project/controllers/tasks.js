3c526754f5f991da95e01c63695dc056dca2ad73
atwanzireT <tatwanzire@gmail.com>
2022-05-30 08:03:12 +0300

    workedd on the get one , get all  and delete functions

--- Task-manager-project/controllers/tasks.js
+++ Task-manager-project/controllers/tasks.js
@@ -1,7 +1,12 @@
 const Task = require("../modals/tasks");
 
-const getAllTasks = (req, res) => {
-    res.send("All Tasks ... ")
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json(tasks);
+    } catch (error) {
+        res.status(500).send({ message: error });
+    }
 }
 
 const createTask = async (req, res) => {
@@ -13,16 +18,34 @@ const createTask = async (req, res) => {
     }
 }
 
-const getTask = (req, res) => {
-    res.json({ id: req.params.id })
+const getTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id:taskID});
+        if(!task) {
+            return res.status(404).send({message: "Task not found"});
+        }
+        res.status(201).json(task);
+    } catch (error) {
+        res.status(500).send({ message: error });
+    }
 }
 
 const updateTask = (req, res) => {
     res.send("Update Task ... ")
 }
 
-const deleteTask = (req, res) => {
-    res.send("Delete Task ... ")
+const deleteTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params;
+        const task = await Task.findOneAndDelete({_id:taskID});
+        if(!task){
+            return res.status(404).json({msg: `Task not found with id ${taskID}`});
+        }
+        res.status(201).json(task);
+    } catch (error) {
+        res.status(500).send({ message: error });
+    }
 }
 
 module.exports = {
